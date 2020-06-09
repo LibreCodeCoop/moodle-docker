00f9847b5bc4f5ce9595e80e04903eb1c3837e6d
LiviaGouvea <gouvea.lc@gmail.com>
2020-06-09 12:20:28 -0300

    Insert creation of data directory

--- .docker/php7/entrypoint.sh
+++ .docker/php7/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ ! -f "web/admin/cli/cron.php" ]; then
-    git clone --progress -b "${MOODLE_VERSION}" --single-branch --depth 1 --recurse-submodules -j 4 git://git.moodle.org/moodle.git /tmp/moodle
+    git clone --progress -b "${MOODLE_VERSION}" --single-branch --depth 1 -j 4 git://git.moodle.org/moodle.git /tmp/moodle
     rsync -r /tmp/moodle/ web
     rm -rf /tmp/moodle
     cp web/config-dist.php web/config.php
@@ -10,6 +10,7 @@ if [ ! -f "web/admin/cli/cron.php" ]; then
     sed -i "s/\$CFG->dbpass    = 'password';/\$CFG->dbpass    = '$POSTGRES_PASSWORD';/g" web/config.php
     sed -i "s@\$CFG->wwwroot   = 'http://example.com/moodle';@\$CFG->wwwroot   = '$BASE_URL';@g" web/config.php
     sed -i "s@\$CFG->dataroot  = '/home/example/moodledata';@\$CFG->dataroot  = '$DATA_PATH';@g" web/config.php
-    chown -R www-data:www-data data
+    mkdir $DATA_PATH
+    chown -R www-data:www-data $DATA_PATH
 fi
 php-fpm
